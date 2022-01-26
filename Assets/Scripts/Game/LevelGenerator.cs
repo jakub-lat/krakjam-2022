be7b744854e54f40146c0d79a1eb1c7cc0ba1a54
Jakub <jakub@cubepotato.eu>
2022-01-26 17:03:44 +0100

    simple level generator

---
+++ Assets/Scripts/Game/LevelGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Cyberultimate.Unity;
+using UnityEngine;
+
+namespace Game
+{
+    public class LevelGenerator : MonoSingleton<LevelGenerator>
+    {
+        [SerializeField] private List<LevelPart> levelParts;
+        [SerializeField] private float levelPartSize;
+        private Vector3 bottomLeft;
+        
+        public void GenerateLevel(float difficulty)
+        {
+            // var level = new GameObject[3, 3];
+            var parts = new List<LevelPart>();
+            for (var i = 0; i < 9; i++)
+            {
+                var part = levelParts[Random.Range(0, levelParts.Count - 1)];
+                Debug.Log(part.name);
+                
+                var pos = bottomLeft;
+                int x = i / 3, z = i % 3;
+                pos.x += levelPartSize * x;
+                pos.z += levelPartSize * z;
+
+                parts.Add(Instantiate(part, pos, Quaternion.identity, transform));
+            }
+            
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Game/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Cyberultimate.Unity;
+using UnityEngine;
+
+namespace Game
+{
+    public class LevelManager : MonoSingleton<LevelManager>
+    {
+        [SerializeField] private int levelCount;
+        [SerializeField] private AnimationCurve difficulty;
+
+        public int CurrentLevel { get; private set; }
+
+        private void Start()
+        {
+            LevelGenerator.Current.GenerateLevel(difficulty.Evaluate(CurrentLevel / levelCount));
+        }
+
+        public void NextLevel()
+        {
+            CurrentLevel++;
+            LevelGenerator.Current.GenerateLevel(difficulty.Evaluate(CurrentLevel / levelCount));
+        }
+    }
+}
