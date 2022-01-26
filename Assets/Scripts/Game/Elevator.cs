442ddf80c9de22d2e0eaa5fbf05c8360752d69f9
Jakub <jakub@cubepotato.eu>
2022-01-26 23:51:16 +0100

    elevator animation

--- Assets/Scripts/Game/Elevator.cs
+++ Assets/Scripts/Game/Elevator.cs
@@ -1,16 +1,44 @@
 ﻿using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Game
 {
     public class Elevator : MonoBehaviour
     {
+        [SerializeField] private Transform doorsLeft, doorsRight;
+        [SerializeField] private Vector3 doorsLeftOpenLocalPos, doorsRightOpenLocalPos;
+        [SerializeField] private float animDuration, closeDelay;
+        
+        private Vector3 doorsLeftClosedLocalPos, doorsRightClosedLocalPos;
+
+        private void Start()
+        {
+            doorsLeftClosedLocalPos = doorsLeft.localPosition;
+            doorsRightClosedLocalPos = doorsRight.localPosition;
+        }
+
         public void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
-                LevelManager.Current.NextLevel();
+
+                Toggle(true);
+                // LevelManager.Current.NextLevel();
             }
+        }
+
+        public void Use()
+        {
+            Toggle(false);
+        }
+        
+        private void Toggle(bool isOpen)
+        {
+            doorsLeft.DOLocalMove(isOpen ? doorsLeftOpenLocalPos : doorsLeftClosedLocalPos, animDuration)
+                .SetEase(Ease.InOutQuint).SetDelay(isOpen ? 0 : closeDelay);
+            doorsRight.DOLocalMove(isOpen ? doorsRightOpenLocalPos : doorsRightClosedLocalPos, animDuration)
+                .SetEase(Ease.InOutQuint).SetDelay(isOpen ? 0 : closeDelay);
         }
     }
 }
---
+++ Assets/Scripts/Game/ElevatorCloseTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class ElevatorCloseTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                GetComponentInParent<Elevator>().Use();
+            }
+        }
+    }
+}
