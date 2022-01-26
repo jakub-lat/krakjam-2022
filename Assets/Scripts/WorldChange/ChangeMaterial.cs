dadc16bc246b7877eb942b471f139d8544e56a5d
Jakub <jakub@cubepotato.eu>
2022-01-26 12:05:40 +0100

    world change controller - podstawy

---
+++ Assets/Scripts/WorldChange/ChangeMaterial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using KrakJam2022.Player;
+using UnityEngine;
+
+namespace WorldChange
+{
+    public class ChangeMaterial : WorldChangeLogic
+    {
+        [SerializeField] private WorldTypeDict<Material> materials;
+        private MeshRenderer meshRenderer;
+
+        private void Awake()
+        {
+            meshRenderer = GetComponent<MeshRenderer>();
+        }
+
+        public override void OnWorldTypeChange(WorldTypeController.WorldType type)
+        {
+            meshRenderer.material = materials[type];
+        }
+    }
+}
---
+++ Assets/Scripts/WorldChange/WorldChangeLogic.cs
@@ -0,0 +1,10 @@
+﻿using KrakJam2022.Player;
+using UnityEngine;
+
+namespace WorldChange
+{
+    public abstract class WorldChangeLogic : MonoBehaviour
+    {
+        public abstract void OnWorldTypeChange(WorldTypeController.WorldType type);
+    }
+}
