f30c701c3de66fe3feff04dffca2f2c0d28c9240
jatoja <fikulatu@gmail.com>
2022-01-27 15:42:17 +0100

    Generation works*                  (*maybe)

--- Assets/Scripts/FloorGeneration/GenerateRoom.cs
+++ Assets/Scripts/FloorGeneration/GenerateRoom.cs
@@ -40,6 +40,7 @@ public class GenerateRoom : MonoBehaviour
     public List<GameObject> puzzleObj;
     public int height=1, width = 1;
     public float spaceX = 1, spaceZ = 1;
+    public GameObject boundaryWall;
 
     private void Start()
     {
@@ -55,6 +56,19 @@ public class GenerateRoom : MonoBehaviour
     {
         float z = 0;
         Debug.Log("rows: "+f.rows.Count);
+
+        for(float i = 0; i < width * spaceX*4; i += spaceX)
+        {
+            Instantiate(boundaryWall, new Vector3(i, 0, 0), Quaternion.Euler(new Vector3(0, 180, 0)), transform);
+            Instantiate(boundaryWall, new Vector3(i, 0, spaceZ*height*2-1), Quaternion.Euler(new Vector3(0, 0, 0)), transform);
+        }
+
+        for(float i = 0; i < spaceZ * height*2; i += spaceZ)
+        {
+            Instantiate(boundaryWall, new Vector3(0, 0, i), Quaternion.Euler(new Vector3(0, 270, 0)), transform);
+            Instantiate(boundaryWall, new Vector3(width * spaceX * 4-1, 0, i), Quaternion.Euler(new Vector3(0, 90, 0)), transform);
+        }
+
         foreach(Row r in f.rows)
         {
             float x = 0;
@@ -62,28 +76,37 @@ public class GenerateRoom : MonoBehaviour
             {
                 Instantiate(s.a.prefab, new Vector3(x, 0, z), Quaternion.Euler(Vector3.zero), transform);
                 Instantiate(s.b.prefab, new Vector3(x+3*spaceX,0, z+spaceZ), Quaternion.Euler(new Vector3(0,180,0)), transform);
+
                 x += 4 * spaceX;
             }
             z += 2*spaceZ;
         }
     }
 
+    private List<Square> allSq;
+    private List<Row> allRows;
+    bool first = true;
     public Floor GenerateStruct(int height, int width)
     {
-        List<Square> allSq = GetAllL(fragments);
-        foreach(Square s in allSq)
+        if (first)
         {
-            if (leftSqMask.ContainsKey(s.mleft)) leftSqMask[s.mleft].Add(s);
-            else leftSqMask[s.mleft] = new List<Square>() { s };
+            allSq = GetAllL(fragments);
+            foreach (Square s in allSq)
+            {
+                if (leftSqMask.ContainsKey(s.mleft)) leftSqMask[s.mleft].Add(s);
+                else leftSqMask[s.mleft] = new List<Square>() { s };
 
+            }
+
+            allRows = GetAllRows(allSq, width);
+            foreach (Row r in allRows)
+            {
+                if (topRowMask.ContainsKey(r.mtop)) topRowMask[r.mtop].Add(r);
+                else topRowMask[r.mtop] = new List<Row>() { r };
+            }
+            first = false;
         }
 
-        List<Row> allRows = GetAllRows(allSq, width);
-        foreach(Row r in allRows)
-        {
-            if (topRowMask.ContainsKey(r.mtop)) topRowMask[r.mtop].Add(r);
-            else topRowMask[r.mtop] = new List<Row>() { r };
-        }
         Debug.Log(allRows.Count);
         Floor f = GetFloor(allRows,height);
 
@@ -190,7 +213,7 @@ public class GenerateRoom : MonoBehaviour
                         else if (d == 8) newS.mdown = 1;
                         newS.mdown += ((next.mshort + curr.mlong) << 2) & ((1 << 4) - 1);
 
-                        Debug.Log(s + " gut");
+                        Debug.Log(s + " gut reverse");
                         res.Add(newS);
                     }
                 }
@@ -208,42 +231,61 @@ public class GenerateRoom : MonoBehaviour
 
     public List<List<Square>> GetAllSqChains(List<List<Square>> prevOpt, int size, int endSize, int limit)
     {
-
         List<List<Square>> res = new List<List<Square>>();
-        int cLimit = 0;
-
-        int i = 0;
-        foreach (List<Square> l in prevOpt) //start row with this square
+        while (size < endSize)
         {
-            Square curr = l[size - 1];
-            Debug.Log("mr: "+curr.mright);
-            for (int S = curr.mright; S > 0; S = (S - 1) & curr.mright) //every submask
-            {
-                Debug.Log("S: "+S);
-                if(!leftSqMask.ContainsKey(S)) continue;
-                
-                var newList = new List<Square>(l);
-                int j = Random.Range(0, leftSqMask[S].Count);
-                newList.Add(leftSqMask[S][j]);
-                cLimit++;
-                res.Add(newList);
-                Debug.Log(S.ToString() + " mask is gut");
+            int cLimit = 0;
 
+            foreach (List<Square> l in prevOpt) //start row with this square
+            {
+                Square curr = l[size - 1];
+                Debug.Log("mr: " + curr.mright);
+                for (int S = curr.mright; S > 0; S = (S - 1) & curr.mright) //every lower submask
+                {
+                    Debug.Log("S: " + S);
+                    if (!leftSqMask.ContainsKey(S)) continue;
+
+                    var newList = new List<Square>(l);
+                    int j = Random.Range(0, leftSqMask[S].Count);
+                    newList.Add(leftSqMask[S][j]);
+                    cLimit++;
+                    res.Add(newList);
+                    Debug.Log(S.ToString() + " mask is gut");
+
+                    if (cLimit >= limit) break;
+                }
+                if (cLimit >= limit) break;
+                for (int S = curr.mright+1; S < 3; S++) //every higher submask
+                {
+                    if (S == 2 && curr.mright == 1) S += 1;
+
+                    Debug.Log("S: " + S);
+                    if (!leftSqMask.ContainsKey(S)) continue;
+
+                    var newList = new List<Square>(l);
+                    int j = Random.Range(0, leftSqMask[S].Count);
+                    newList.Add(leftSqMask[S][j]);
+                    cLimit++;
+                    res.Add(newList);
+                    Debug.Log(S.ToString() + " mask is gut");
+
+                    if (cLimit >= limit || S == 3) break;
+                }
                 if (cLimit >= limit) break;
             }
-            i++;
-            if (cLimit >= limit) break;
+
+            Debug.Log("ii " + res.Count.ToString());
+
+            prevOpt = res;
+            size++;
+            res = new List<List<Square>>();
         }
 
-        if (size+1 >= endSize) return res;
-        Debug.Log("ii " + res.Count.ToString());
-
-        res = GetAllSqChains(res, size + 1, endSize, limit);
-
-        return res;
+        Debug.Log("ii " + prevOpt.Count.ToString());
+        return prevOpt;
     }
 
-    public List<Row> GetAllRows(List<Square> frag, int size, int limit=100)
+    public List<Row> GetAllRows(List<Square> frag, int size, int limit=10000)
     {
         if (size > 8) size = 8;
 
@@ -273,17 +315,14 @@ public class GenerateRoom : MonoBehaviour
             int i = 0;
             foreach(Square s in sl)
             {
-                mtop += s.mtop << 4*i;
-                mdown += s.mdown << 4*i;
+                mtop += s.mtop << (4*i);
+                mdown += s.mdown << (4*i);
                 i++;
             }
             newRow.mtop = mtop;
             newRow.mdown = mdown;
 
             res.Add(newRow);
-
-            Debug.Log("top: " + mtop);
-            Debug.Log("bot: " + mdown);
         }
 
 
@@ -295,46 +334,101 @@ public class GenerateRoom : MonoBehaviour
         List<Row> sel = new List<Row>();
 
         int currSize = 1;
-        Row curr = frag[Random.Range(0, frag.Count)];
+        int ir = Random.Range(0, frag.Count);
+        Row curr = frag[ir];
         List<int> myMasks = new List<int>();
         sel.Add(curr);
+
+        int endP = (int)Mathf.Pow(2, 4*size-1);
+
+        int limit = 10000;
+        int cLimit = 0;
+
+        int limit2 = 10000;
+        int cLimit2 = 0;
+
+        Dictionary<int, List<int>> downMasks = new Dictionary<int, List<int>>();
+
+        bool masksHigher = true;
+        bool masksLower = true;
 
         while (currSize < size)
         {
             myMasks = new List<int>();
 
-            for (int S = curr.mdown; S > 0; S = (S - 1) & curr.mdown) //every submask
-            {
-                myMasks.Add(S);
-            }
+                cLimit = 0;
+                if (masksLower)
+                {
+                    for (int S = curr.mdown; S > 0; S = (S - 1) & curr.mdown) //every lower submask
+                    {
+                        myMasks.Add(S);
+                        cLimit++;
+                        if (cLimit >= limit) break;
+                    }
+                }
+                if (masksHigher)
+                {
+                    int Sh;
+                    for (int S = ~curr.mdown; S < 0; S = (S + 1) & curr.mdown) //every HIGHER submask
+                    {
+                        Sh = ~S;
+                        myMasks.Add(Sh);
 
-            int currM=0, i=0;
-            if (myMasks.Count > 0)
+                        if (S == endP) break;
+                        cLimit++;
+                        if (cLimit >= limit) break;
+                    }
+                }
+
+            int currM=-1, i=0;
+            bool bad = (myMasks.Count <= 0);
+
+            if (!bad)
             {
                 i = Random.Range(0, myMasks.Count);
                 currM = myMasks[i]; //get random submask
             }
 
 
-            while (myMasks.Count > 0 && (!topRowMask.ContainsKey(currM) || topRowMask[currM].Count <= 0)) // while the curr submask is not good get next one and check
+            while (!bad && myMasks.Count > 0 && (!topRowMask.ContainsKey(currM) || topRowMask[currM].Count <= 0)) // while the curr submask is not good get next one and check
             {
                 myMasks.RemoveAt(i);
                 i = Random.Range(0, myMasks.Count);
+
+                if (myMasks.Count <= 0) break;
                 currM = myMasks[i]; //get random submask
             }
 
-            if(myMasks.Count<=0 && (!topRowMask.ContainsKey(currM) || topRowMask[currM].Count <= 0)) //if still no submask good 
+            if(bad || (myMasks.Count<=0 && (!topRowMask.ContainsKey(currM) || topRowMask[currM].Count <= 0))) //if still no submask good 
             {
                 sel.RemoveAt(currSize - 1);
-                int r = Random.Range(0, frag.Count); //try next row
-                curr = frag[r];
-                sel.Add(frag[r]);
+                ir = Random.Range(0, frag.Count); //try next row
+                curr = frag[ir];
+                sel.Add(curr);
             } else
             {
-                Row newR = topRowMask[currM][Random.Range(0, topRowMask[currM].Count)];
+                int j = Random.Range(0, topRowMask[currM].Count);
+                Row newR = topRowMask[currM][j];
+
+                Debug.Log("New row added with botM: " + currM + " at index: " + j);
+                string s = "Row contains: ";
+                foreach(Square sq in newR.squares)
+                {
+                    s += "{" + sq.a.prefab.name + ", " + sq.b.prefab.name + "}, ";
+                }
+                Debug.Log(s);
                 sel.Add(newR);
                 curr = newR;
                 currSize++;
+
+                cLimit2 = 0;
+            }
+
+            cLimit2++;
+            if (cLimit2 >= limit2)
+            {
+                Debug.LogError("Row Checking limit reached");
+                break;
             }
         }
 
