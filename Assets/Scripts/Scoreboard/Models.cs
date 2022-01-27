05819de23351d041dc0020b61dc4c00dd0cb7f55
Jakub <jakub@cubepotato.eu>
2022-01-27 15:28:20 +0100

    scoreboard + add bcrypt

---
+++ Assets/Scripts/Scoreboard/Models.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Scoreboard
+{
+    [Serializable]
+    public class Player
+    {
+        public long id;
+        public string name;
+        public string token;
+        public GameRun[] gameRuns;
+    }
+
+    [Serializable]
+    public class GameRun
+    {
+        public long id;
+        public long playerID;
+        public long startTime;
+        public long endTime;
+        public int kills;
+        public int headshots;
+        public int deaths;
+        public GameRunLevel[] levels;
+    }
+
+    [Serializable]
+    public class GameRunLevel
+    {
+        public int id;
+        public int gameRunID;
+        public long startTime;
+        public long endTime;
+        public int level;
+        public int kills;
+        public int headshots;
+        public int deaths;
+
+    }
+}
