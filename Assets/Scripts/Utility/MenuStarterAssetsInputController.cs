1bf5262ad957a1c9755e17be57a7ee674dd0e22a
TheMatiaz0 <TheMatiaz0@protonmail.com>
2022-01-28 16:47:02 +0100

    Screenshots support

---
+++ Assets/Scripts/Utility/MenuStarterAssetsInputController.cs
@@ -0,0 +1,10 @@
+using Cyberultimate.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuStarterAssetsInputController : MonoSingleton<MenuStarterAssetsInputController>
+{
+}
