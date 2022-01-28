a8790adadb378157ee760c6edac2d4ad424ff8bd
Jakub <jakub@cubepotato.eu>
2022-01-28 16:39:04 +0100

    Keyboard as a Weapon (KaaW)

--- Assets/Scripts/UsableItems/UsableItem.cs
+++ Assets/Scripts/UsableItems/UsableItem.cs
@@ -7,6 +7,9 @@ namespace UsableItems
     [RequireComponent(typeof(Rigidbody))]
     public abstract class UsableItem : InteractiveObject
     {
+        public Vector3 rotationOffset;
+        public Vector3 positionOffset;
+        
         public abstract void Use();
 
         public virtual void OnPickup()
