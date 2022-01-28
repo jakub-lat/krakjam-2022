4f5c640f7e139ef141be19be1af7b702f9cf9496
jatoja <fikulatu@gmail.com>
2022-01-28 15:11:32 +0100

    Elevator has no walls behind

--- Assets/Scripts/Game/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager.cs
@@ -27,12 +27,14 @@ namespace Game
 
         private void Start()
         {
-            (startingElevator, finishElevator) = (finishElevator, startingElevator);
+            (startingElevator, finishElevator, elevator1Z) = (finishElevator, startingElevator, elevator2Z);
             width = GenerateRoom.Current.width;
             height = GenerateRoom.Current.height;
             spaceX = GenerateRoom.Current.spaceX;
             spaceZ = GenerateRoom.Current.spaceZ;
-            
+
+            elevator2Z = (int) (startingElevator.transform.position.z / spaceZ)+1;
+
             if (startingPosA != null)
             {
                 player.position = startingPosA.position;
@@ -48,7 +50,7 @@ namespace Game
         {
             CurrentLevel++;
 
-            (startingElevator, finishElevator) = (finishElevator, startingElevator);
+            (startingElevator, finishElevator, elevator1Z) = (finishElevator, startingElevator, elevator2Z);
 
             Scoreboard.Scoreboard.Current.PostLevelData();
 
@@ -62,14 +64,17 @@ namespace Game
             finishElevator.active = true;
         }
 
+        private int elevator1Z;
+        private int elevator2Z;
         private void GenerateLevel()
         {
-            Vector3 newElevatorPos = new Vector3(finishElevator.transform.position.x, finishElevator.transform.position.y,UnityEngine.Random.Range(0,height*2)*spaceZ);
+            elevator2Z = UnityEngine.Random.Range(0, height * 2);
+            Vector3 newElevatorPos = new Vector3(finishElevator.transform.position.x, finishElevator.transform.position.y,elevator2Z*spaceZ);
 
             finishElevator.transform.position = newElevatorPos;
 
             GenerateRoom.Current.transform.KillAllChildren();
-            GenerateRoom.Current.Generate();
+            GenerateRoom.Current.Generate(elevator1Z, elevator2Z);
             ObjectGeneration.Current.GenerateObjects();
             Debug.Log("done");
         }
