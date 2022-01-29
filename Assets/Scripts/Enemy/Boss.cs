c5940ee0ca2e8f238a4353506a180a5288bb3c7a
jatoja <fikulatu@gmail.com>
2022-01-29 11:30:36 +0100

    Boss shooting :o

--- Assets/Scripts/Enemy/Boss.cs
+++ Assets/Scripts/Enemy/Boss.cs
@@ -13,6 +13,25 @@ public class Boss : MonoSingleton<Boss>
     public float startingHealth = 600;
     public Image healthBar;
     public Canvas BossUI;
+    public Transform gunRotate;
+    public Transform gunPoint;
+    public CharacterController playerCharacter;
+
+    private Transform player;
+
+
+    [Header("Shooting")]
+    public string bulletPoolTag = "EnemyBullet";
+    public int burstSize = 15;
+    public float damage = 10f;
+    public float burstFireRate = 0.05f;
+    public float fireRate = 2f;
+    public float dispersion = 40;
+    public float bulletSpeed = 300f;
+
+    public bool shootInFront = true;
+    public float inFrontMultiplier = 1f;
+    
 
     public void StartBattle()
     {
@@ -28,11 +47,40 @@ public class Boss : MonoSingleton<Boss>
     private void Start()
     {
         BossUI.enabled = false;
+        player = PlayerInstance.Current.transform;
     }
 
+    private int currBurst = 0;
+    private float burstTimer = 0;
+    private float shootingTimer = 0;
     private void Update()
     {
         if (dead || !battle) return;
+
+        var look = playerCharacter.transform.position;
+        gunRotate.transform.LookAt(look);
+        look.y = transform.position.y;
+        transform.LookAt(look);
+
+        if (currBurst <= 0)
+        {
+            burstTimer -= Time.deltaTime;
+            if (burstTimer <= 0)
+            {
+                burstTimer = fireRate;
+                currBurst = burstSize;
+                shootingTimer = fireRate;
+            }
+        } else
+        {
+            shootingTimer -= Time.deltaTime;
+            if (shootingTimer <= 0)
+            {
+                shootingTimer = burstFireRate;
+                currBurst--;
+                Shoot();
+            }
+        }
     }
 
     public void GotHit(float amount)
@@ -47,5 +95,25 @@ public class Boss : MonoSingleton<Boss>
         }
 
         healthBar.fillAmount = health/ startingHealth;
+    }
+
+    void Shoot()
+    {
+        GameObject obj = ObjectPooler.Current.SpawnPool(bulletPoolTag, gunPoint.position, Quaternion.identity);
+
+        var vel = playerCharacter.velocity;
+        Debug.Log(vel);
+        Vector3 inFront = player.position + vel * inFrontMultiplier;
+        Debug.Log("p: " + inFront);
+
+        
+        Vector3 dir = ((shootInFront ? inFront : player.position) - gunPoint.position).normalized * bulletSpeed;
+        Vector3 dispDir = new Vector3(Random.Range(-dispersion, dispersion), Random.Range(-dispersion, dispersion), Random.Range(-dispersion, dispersion));
+
+        Debug.Log("dir: " + dir);
+
+        obj.transform.forward = dir+dispDir;
+        obj.GetComponent<Rigidbody>().AddForce(dir+dispDir);
+        obj.GetComponent<EnemyBullet>().damage = damage;
     }
 }
