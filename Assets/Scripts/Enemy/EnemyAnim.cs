a4f185c0c5850846066577ae2d1b8885c27ef56e
Jakub <jakub@cubepotato.eu>
2022-01-29 22:12:53 +0100

    enemy anim

---
+++ Assets/Scripts/Enemy/EnemyAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnim : MonoBehaviour
+{
+    private Enemy e;
+
+    private void Start()
+    {
+        e = GetComponentInParent<Enemy>();
+    }
+    public void Kaput()
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+
+    public void EndHit()
+    {
+        e.HitEnd();
+    }
+}
