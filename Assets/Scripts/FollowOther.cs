5ad817a36fe1af00b98f1a96be238b285c3be5b6
Jakub <jakub@cubepotato.eu>
2022-01-29 01:02:08 +0100

    hand animations

---
+++ Assets/Scripts/FollowOther.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class FollowOther : MonoBehaviour
+    {
+        [SerializeField] private Transform other;
+
+        private void Update()
+        {
+            transform.position = other.position;
+            transform.rotation = other.rotation;
+        }
+    }
+}
